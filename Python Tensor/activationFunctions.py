73ee6cf229e4dbc7f90170b8335ddbcdad5e70cd
yousefessam <yousef.essam.eldin@gmail.com>
2018-06-03 00:10:14 +0200

    Activation function in Tensor Flow basics

---
+++ Python Tensor/activationFunctions.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  2 23:20:58 2018
+
+@author: Yousef Essam
+"""
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot_act(i=1.0, actfunc=lambda x: x):
+    ws = np.arange(-0.5, 0.5, 0.1)
+    bs = np.arange(-0.5, 0.5, 0.1)
+
+    X, Y = np.meshgrid(ws, bs)
+    os = np.array([actfunc(tf.constant(w*i + b)).eval(session=sess) \
+                   for w,b in zip(np.ravel(X), np.ravel(Y))])
+
+    Z = os.reshape(X.shape)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1)
+
+#start a session
+sess = tf.Session();
+#create a simple input of 3 real values
+i = tf.constant([1.0, 2.0, 3.0], shape=[1, 3])
+#create a matrix of weights
+w = tf.random_normal(shape=[3, 3])
+#create a vector of biases
+b = tf.random_normal(shape=[1, 3])
+#dummy activation function
+def func(x): return x
+#tf.matmul will multiply the input(i) tensor and the weight(w) tensor then sum the result with the bias(b) tensor.
+bb = sess.run(b)
+ww = sess.run(w)
+act = func(tf.matmul(i, ww) + bb)
+#Evaluate the tensor to a numpy array
+act.eval(session=sess)
+
+plot_act(1.0, func)
+
+plot_act(1, tf.sigmoid)
+
+
+# Using sigmoid in a neural net layer
+act = tf.sigmoid(tf.matmul(i, ww) + b)
+act.eval(session=sess)
+
+plot_act(1, tf.tanh)
+
+#Using tanh in a neural net layer
+act = tf.tanh(tf.matmul(i, ww) + b)
+act.eval(session=sess)
+
+
+
+plot_act(1, tf.nn.relu)
+
+#Using relu in a neural net layer 
+# it takes care of the Vanishing and Exploding Gradient 
+act = tf.nn.relu(tf.matmul(i, ww) + b)
+act.eval(session=sess)
